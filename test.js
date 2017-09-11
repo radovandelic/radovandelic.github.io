09edcf02fa4dd5e4938449f94661cd8debf4dbb1
Radovan Delic <infamouszgbgd@yahoo.com>
2017-09-11 16:28:48 +0200

    scope

--- test.js
+++ test.js
@@ -1,3 +1,13 @@
-console.log('abcd');
+//console.log('abcd');
 //random change
 //another random change
+
+var obj = {
+	prop: 'propobj',
+	whatnow: function(){
+		//var prop = 'propinside';
+		console.log(this.prop);
+	}
+
+}
+obj.whatnow()
