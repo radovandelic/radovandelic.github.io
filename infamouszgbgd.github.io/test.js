eb53eb5ce5ae08fe0e50500cd1cde70e41537f7d
Radovan Delic <infamouszgbgd@yahoo.com>
2017-09-12 15:08:51 +0200

    index

---
+++ infamouszgbgd.github.io/test.js
@@ -0,0 +1,13 @@
+//console.log('abcd');
+//random change
+//another random change
+
+var obj = {
+	prop: 'propobj',
+	whatnow: function(){
+		//var prop = 'propinside';
+		console.log(this.prop);
+	}
+
+}
+obj.whatnow();
