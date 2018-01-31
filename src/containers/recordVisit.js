9b745a1e93428aa792d147543b8a7b634a7367e3
Radovan Delic <infamouszgbgd@yahoo.com>
2018-01-31 19:59:25 +0100

    bs

--- src/containers/recordVisit.js
+++ src/containers/recordVisit.js
@@ -2,22 +2,20 @@ const recordVisit = (page) => {
     fetch('https://geoip-db.com/json/geoip.php')
         .then(res => { return res.json(); })
         .then(res => {
-            if (res.IPv4 !== '109.134.146.127') {
-                var q = {
-                    method: 'POST',
-                    body: {
-                        location: res.city ? `${res.city} ${res.postal}, ${res.country_code}` : res.country_name,
-                        page: page,
-                        browser: window.navigator.userAgent,
-                        ip: res.IPv4,
-                        time: Date(Date.now()).toLocaleString().substring(0, 24)
-                    }
+            var q = {
+                method: 'POST',
+                body: {
+                    location: res.city ? `${res.city} ${res.postal}, ${res.country_code}` : res.country_name,
+                    page: page,
+                    browser: window.navigator.userAgent,
+                    ip: res.IPv4,
+                    time: Date(Date.now()).toLocaleString().substring(0, 24)
                 }
-                q.body = JSON.stringify(q.body);
-                fetch(`https://battleshipsjs.herokuapp.com/visit/`, q)
-                    .catch(err => { console.log(err) });
-
             }
+            q.body = JSON.stringify(q.body);
+            fetch(`https://battleshipsjs.herokuapp.com/visit/`, q)
+                .catch(err => { console.log(err) });
+
         })
 }
 
